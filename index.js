6291e4fc4519e481d3dbfcc4e1a3f2723eae64c9
KristinaKopylova <kkopylova767@gmail.com>
2025-11-15 04:07:02 +0000

    {Что делали}

--- index.js
+++ index.js
@@ -1,51 +1,76 @@
-const db = require("./db");
-const express = require("express");
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const db = require("./db")
+const express = require("express")
 
-const app = express();
+const salt = "secret-key"
+const SECRET = "this-is-for-JWT"
 
-app.use(express.json());
+const app = express()
 
-// Добавляем пользователя
-const insertUser = db.prepare(
-  'INSERT INTO users (username, email, password, role) VALUES (?, ?, ?, ?)'
-);
-const result = insertUser.run('Alice', 'alice@example.com', 'password!', 'user');
+app.use(express.json())
 
-const lastInsertedId = result.lastInsertRowid;
+// Добавка пользователя
+app.post("/api/auth/register", (req, res) => {
+  const { email, name, password, role } = req.body
 
-// Добавляем книгу
-const insertBook = db.prepare(
-  'INSERT INTO books (title, author, year, genre, description, createdBy) VALUES (?, ?, ?, ?, ?, ?)'
-);
-insertBook.run('The Great Adventure', 'Alice Johnson', 2023, 'Fantasy', 'A great adventure story', lastInsertedId);
+    try {
+        if (!email || !name || !password || !role) {
+            return res.status(400).json({ error: "Не хватает данных" })
+        }
+        const syncSalt = bcrypt.genSaltSync(10)
+        const hashed = bcrypt.hashSync(password, syncSalt)
+        const query = db.prepare(
+            `INSERT INTO users (email, name, password, role) VALUES (?, ?, ?, ?)`
+        )
+        const info = query.run(email, name, hashed)
+        const newUser = db
+            .prepare(`SELECT * FROM users WHERE ID = ?`)
+            .get(info.lastInsertRowid)
+        res.status(201).json(newUser)
+    } catch (error) {
+        console.error(error)
+        res.status(401).json({error: "Неправильный токен"})
+    }
+});
+//получение информации о пользователе
+app.get("/api/auth/profile", (req, res) => {
+  const lastInsertedId = result.lastInsertRowid;
+});
 
-console.log(`User added with ID ${lastInsertedId}`);
+// Добавка книгу
+app.post("/api/books", (req, res) => {const insertBook = db.prepare(
+    'INSERT INTO books (title, author, year, genre, description, createdBy) VALUES (?, ?, ?, ?, ?, ?)');
+  insertBook.run('The Great Adventure', 'Alice Johnson', 2023, 'Fantasy', 'A great adventure story', lastInsertedId);
 
-// Выборка книг с информацией о пользователе, который их создал
-const booksWithUsers = db
-  .prepare(
-    `
-      SELECT books.id, books.title, books.author, users.username AS added_by 
-      FROM books 
-      JOIN users ON books.createdBy = users.id
-    `
-  )
-  .all();
+  console.log(`User added with ID ${lastInsertedId}`); //сообщение о добавленном пользователе
+});
 
-console.log(booksWithUsers);
-
+// Вce books
+app.get("/api/books", (req, res) => {
+  const booksWithUsers = db
+    .prepare(
+      `
+        SELECT books.id, books.title, books.author, users.username AS added_by 
+        FROM books 
+        JOIN users ON books.createdBy = users.id
+      `
+    )
+    .all();
+  console.log(booksWithUsers);
+});
+//поиск пользователя по почте
 const email = 'alice@example.com';
 const user = db
   .prepare('SELECT * FROM users WHERE email = ?')
   .get(email);
-
 if (user) {
   console.log(`Found user: ${JSON.stringify(user)}`);
 } else {
   console.log('No user found for the given email.');
 }
 
-// Простая аутентификационная мидлварка
+//мидлварка аутен
 const auth = (req, res, next) => {
   const token = req.headers.authorization?.split(' ')[1];
 
@@ -53,13 +78,13 @@ const auth = (req, res, next) => {
     return res.status(401).json({ message: 'Unauthorized' });
   }
 
-  // Имитация успешного входа пользователя
+  //успешный вход пользователя
   req.user = { id: 1, username: 'test-user', role: 'admin' };
 
   next();
 };
 
-// Создаем мидлварку, которая будет проверять роль
+//мидлварку проверка роль
 function checkRole(...allowedRoles) {
   return (req, res, next) => {
     if (!req.user) {
@@ -74,25 +99,23 @@ function checkRole(...allowedRoles) {
   };
 };
 
-// Использование мидлварки checkRole
-app.get('/users', auth, checkRole('admin'), (req, res) => {
-  // Обработчик маршрута для пользователей с ролью администратора
-  res.json({ message: 'Привет администратор!' });
+// для админа проверка роли
+app.get('/api/users', auth, checkRole('admin'), (req, res) => {
+  res.json({ message: 'админ' });
 });
 
-// Альтернативный вариант проверки роли прямо в запросе
-app.post('/users', auth, (req, res) => {
+// 2 вариант проверки роли
+app.post('/api/role', auth, (req, res) => {
   if (!['admin'].includes(req.user.role)) {
     return res
       .status(403)
-      .json({ message: 'Доступ запрещен: недостаточно прав' });
+      .json({ message: 'Доступ запрещен: недостаточно прав(юзер)' });
   }
 
-// Логика обработки POST-запроса для пользователей с ролью администратора
   res.json({ message: 'POST-запрос успешно выполнен!' });
 });
 
-// Запуск сервера Express
+// сервер Express
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
